e11b2e70aadc35480ac9767b48214947e1eb2e0e
wandersontx <wandersonh89@gmail..com>
2021-02-14 20:45:39 -0300

    Gridview e mediaQuery

--- lib/Bichos.dart
+++ lib/Bichos.dart
@@ -6,8 +6,40 @@ class Bichos extends StatefulWidget {
 }
 
 class _BichosState extends State<Bichos> {
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+   
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: [
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset('assets/imagens/cao.png'),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset('assets/imagens/gato.png'),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset('assets/imagens/ovelha.png'),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset('assets/imagens/macaco.png'),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset('assets/imagens/vaca.png'),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset('assets/imagens/leao.png'),
+        ),
+
+      ],
+      );
   }
 }
